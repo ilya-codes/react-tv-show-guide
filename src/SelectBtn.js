eacf48c49fe016f939329545d42cc3b6e1f9a682
Ilya Dmitriev <ilya.d.mailbox@gmail.com>
2022-02-02 16:21:07 +0900

    fix

--- src/SelectBtn.js
+++ src/SelectBtn.js
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledSelect = styled.select`
-  /* width: 13rem; */
+  width: 13rem;
   margin: 0.8rem;
   height: 1.8rem;
   border-radius: 10px;
@@ -14,14 +14,14 @@ const StyledSelect = styled.select`
   cursor: pointer;
   outline: none;
   transition: all 0.2s ease-in-out;
-  text-align: center;
+  padding-left: 1rem;
 
   &:hover {
     background-color: rgba(255, 255, 255, 0.5);
   }
 
   @media screen and (min-width: 900px) {
-    /* width: 9rem; */
+    width: 9rem;
 
     margin: 1.5rem;
   }
